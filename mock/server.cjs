5830fc223ecb2eedc93c9f4be136767efab27e42
shahid-anwar <shahidanwaransari007@gnmail.com>
2026-07-03 14:37:24 +0530

    Fix: add cors require to server

--- mock/server.cjs
+++ mock/server.cjs
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const path = require("path");
-
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
